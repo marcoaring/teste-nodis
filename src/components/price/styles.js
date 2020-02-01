31ec3a3d658cc19035cd9a0db7358ff3928c1d96
Marco Aring <contato@marcoaring.com.br>
2020-02-01 14:53:35 -0300

    feat: created component Price

---
+++ src/components/price/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import media from '../../styles/media-queries';
+
+export const OldPrice = styled.span`
+  color: ${(props) => props.theme.palette.textColor};
+  display: block;
+  font: 12px ${(props) => props.theme.fonts.fontIBMBold};
+  min-height: 15px;
+  text-decoration: line-through;
+`;
+
+export const Price = styled.span`
+  color: ${(props) => props.theme.palette.textColor};
+  display: block;
+  font: 20px ${(props) => props.theme.fonts.fontIBMBold};
+  margin-bottom: 15px;
+`;
