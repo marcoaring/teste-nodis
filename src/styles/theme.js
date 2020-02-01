a7509595c8008ecd701c3f1d81f96a4d0e87017b
Marco Aring <contato@marcoaring.com.br>
2020-02-01 14:51:52 -0300

    feat: inserted new fonts and colors in theme css

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,14 @@
 const theme = {
   fonts: {
-    fontRubik: 'Rubik-Bold'
+    fontRubik: 'Rubik-Bold',
+    fontIBMRegular: 'IBMPlexSans-Regular',
+    fontIBMBold: 'IBMPlexSans-Bold'
   },
   palette: {
     white: '#fff',
     black: '#000',
     primary: '#1c9956',
+    primaryHover: '#37c478',
     textColor: '#333'
   }
 };
