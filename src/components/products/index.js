909e0d414fb2c3b9e707c5783dd0d6e092bfaf16
Marco Aring <contato@marcoaring.com.br>
2020-02-03 23:31:32 -0300

    feat: added condicional if dont have products

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -6,13 +6,19 @@ import ProductCard from '../product-card';
 import * as S from './styles';
 
 export default function Products({ products }) {
-  return (
-    <S.Products>
-      {products.map((product, index) => {
-        return <ProductCard product={ product } key={ index } />;
-      })}
-    </S.Products>
-  );
+  if (products.length > 0) {
+    return (
+      <S.Products>
+        {products.map((product, index) => {
+          return <ProductCard product={ product } key={ index } />;
+        })}
+      </S.Products>
+    );
+  } else {
+    return(
+      <S.Warning>Nenhum Produto encontrado!</S.Warning>
+    );
+  }
 }
 
 Products.propTypes = {
