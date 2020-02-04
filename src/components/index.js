4a76eae37358e29ce7d146beb2b7d9347c606d15
Marco Aring <contato@marcoaring.com.br>
2020-02-04 20:58:47 -0300

    feat: created product detail component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import SEO from './seo';
 import Header from './header';
 import SearchResult from './search-result';
 import Products from './products';
+import ProductDetail from './product-detail';
 import Svgs from './svgs';
 
 export {
@@ -9,5 +10,6 @@ export {
   Header,
   SearchResult,
   Products,
+  ProductDetail,
   Svgs
 };
--- src/pages/produto.js
+++ src/pages/produto.js
@@ -3,17 +3,25 @@ import React, { useState, useEffect } from 'react';
 import {
   SEO,
   Header,
-  SearchResult,
-  Products
+  ProductDetail
 } from '../components';
 
+import { fetchData } from '../services/data';
+
 export default function ProductPage({location}) {
+  const [product, changeProduct] = useState([]);
+
+  useEffect(() => {
+    fetchData(location.state.sku).then((data) => {
+      changeProduct(data);
+    });
+  }, []);
+
   return (
     <>
-      <SEO title="Produto" />
-
-      to aqui - { location.state.sku }
-
+      <SEO title={ product.name } />
+      <Header />
+      <ProductDetail product={ product } />
     </>
   );
 }
