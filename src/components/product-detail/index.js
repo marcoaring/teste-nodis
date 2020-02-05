a5904222c60564911875adb83235fdb407111267
Marco Aring <contato@marcoaring.com.br>
2020-02-04 23:15:36 -0300

    feat: added html in product detail component

--- src/components/product-detail/index.js
+++ src/components/product-detail/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+import Price from '../price';
+import Modal from '../modal';
 
 import * as S from './styles';
 
 export default function ProductDetail({ product }) {
+  const [showModal, changeShowModal] = useState(false);
+  const [status, changeStatus] = useState(false);
+
+  const sendProduct = () => {
+    changeShowModal(true);
+    changeStatus({'error':false});
+  };
+
   return (
     <S.Product>
-      { product.name }
+      <S.BoxImage>
+        <S.Image src={ product.imageUrl } alt={ product.name } />
+      </S.BoxImage>
+
+      <S.Infos>
+        <S.Title>{ product.name }</S.Title>
+
+        <Price promotionalPrice={ product.promotionalPrice } price={ product.salePrice } />
+
+        <S.Description dangerouslySetInnerHTML={{ __html: product.description }}></S.Description>
+
+        <S.Button onClick={ () => sendProduct() }>
+          <S.Text>Comprar</S.Text>
+        </S.Button>
+      </S.Infos>
+
+      <Modal show={ showModal } status={ status } />
     </S.Product>
   );
 }
 
 ProductDetail.propTypes = {
-  product: PropTypes.array.isRequired
+  product: PropTypes.object.isRequired
 };
